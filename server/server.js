798829546a6ec9d4926c228fd570e50a814027ab
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-25 14:11:06 -0600

    update to server to fix async error

--- server/server.js
+++ server/server.js
@@ -8,34 +8,41 @@ const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 // const routes = require('./routes');
+const startServer = async () => {
 
-// build our port, start the app
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-// server for apollo (GraphQL server)
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-// creates and endpoint called /graphql 
-server.applyMiddleware({ app });
 
-// req.body 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // build our port, start the app
+  const PORT = process.env.PORT || 3001;
+  const app = express();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-// app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(` 🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  // server for apollo (GraphQL server)
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
-});
+  // creates and endpoint called /graphql 
+  await server.start();
+  server.applyMiddleware({ app });
+
+  // req.body 
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(` 🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+startServer();
